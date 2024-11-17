715c1d99edc6e5eb34e84689feedb2a5a6150889
oleedee <oneilleedee@gmail.com>
2024-11-17 20:00:28 +0000

    added part of signup api

--- PROJ-2/server.js
+++ PROJ-2/server.js
@@ -25,3 +25,28 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://da804107:<db_password>@proj-2.ghujw.mongodb.net/?retryWrites=true&w=majority&appName=PROJ-2';
 const client = new MongoClient(url);
 client.connect();
+
+app.post('/api/signup', async (req, res, next) => {
+    // incoming, username, pass
+    // outgoing error
+
+    const {username, password} = req.body;
+
+    const newUser = {UserId: null, Username: username, Password: password};
+
+    try {
+    const db = client.db();
+    newUser.UserId = await db.collection('Users').countDocuments();
+    
+    if(await db.collection('Users').findOne({Username: newUser.Username, Password: newUser.Password})) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const results = await db.collection('Users').insertOne(newUser).toArray();
+    } catch(e) {
+        error = e.toString();
+    }
+    
+    var ret = { error: error };
+    res.status(200).json(ret);
+});
