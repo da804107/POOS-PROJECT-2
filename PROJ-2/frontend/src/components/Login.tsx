a21782390e6574a75f3ca0b831b3e2921a5295bd
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-11-20 12:19:56 -0500

    Update Login.tsx

--- PROJ-2/frontend/src/components/Login.tsx
+++ PROJ-2/frontend/src/components/Login.tsx
@@ -23,7 +23,7 @@ function Login() {
   };
   var js = JSON.stringify(obj);
   try {
-    const response = await fetch('http://project.annetteisabrunette.xyz/api/login', {
+    const response = await fetch('https://project.annetteisabrunette.xyz/api/login', {
       method: 'POST',
       body: js,
       headers: {
