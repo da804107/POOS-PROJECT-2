2c2a524445e6ad39be213732ecde61d209d0a7ca
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-11-20 13:51:48 -0500

    Update Login.tsx

--- PROJ-2/frontend/src/components/Login.tsx
+++ PROJ-2/frontend/src/components/Login.tsx
@@ -24,7 +24,7 @@ function Login() {
   var js = JSON.stringify(obj);
     console.log("outside try");
   try {
-    const response = await fetch('https://project.annetteisabrunette.xyz:5000/api/login', {
+    const response = await fetch('https://project.annetteisabrunette.xyz/api/login', {
       method: 'POST',
       body: js,
       headers: {
