c1af98c292b82d0f49e0acfd76f2dfae2dc1f26b
anav24 <navarro.aaronm@gmail.com>
2024-11-21 04:40:31 +0000

    Update App.tsx

--- PROJ-2/frontend/src/App.tsx
+++ PROJ-2/frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage.tsx';
-import HomePage from './pages/HomePage.tsx';
+import HomePage from './pages/HomePage.tsx'
 import StudySetsPage from './pages/StudySetsPage.tsx';
 import CreateAccountPage from './pages/CreateAccountPage.tsx';
 import NewStudySetPage from './pages/NewStudySetPage.tsx';
 import EditStudySetPage from './pages/EditStudySetPage.tsx';
 import IndexCardPage from './pages/IndexCardPage.tsx';
+
+const StudySetsPageWrapper: React.FC = () => {
+  const{ id } = useParams<{ id: string }>();
+  if (!id) return <div>Error: Study set ID not found.</div>
+  return <StudySetsPage studySetId={id} />;
+}
 
 function App() {
 
@@ -16,7 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/homePage" element={<HomePage />} />
-        <Route path="/studySets" element={<StudySetsPage />} />
+        <Route path="/studySets:id" element={<StudySetsPageWrapper />} />
         <Route path="/newAccount" element={<CreateAccountPage />} />
         <Route path="/newStudySet" element={<NewStudySetPage />} />
         <Route path="/editStudySet" element={<EditStudySetPage />} />
