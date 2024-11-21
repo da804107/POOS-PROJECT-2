23c2d5dc3680c2c3e6ed097079ac4ceccb6b4463
anav24 <navarro.aaronm@gmail.com>
2024-11-21 01:17:08 +0000

    Update App.tsx

--- PROJ-2/frontend/src/App.tsx
+++ PROJ-2/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage.tsx';
+import HomePage from './pages/HomePage.tsx';
 import StudySetsPage from './pages/StudySetsPage.tsx';
 import CreateAccountPage from './pages/CreateAccountPage.tsx';
 import NewStudySetPage from './pages/NewStudySetPage.tsx';
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/homePage" element={<HomePage />} />
         <Route path="/studySets" element={<StudySetsPage />} />
         <Route path="/newAccount" element={<CreateAccountPage />} />
         <Route path="/newStudySet" element={<NewStudySetPage />} />
