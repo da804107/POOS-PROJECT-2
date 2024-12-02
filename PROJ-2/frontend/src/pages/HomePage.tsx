e289adc0ba0f08d1d10347c8c84f79d66367b21c
oleedee <oneilleedee@gmail.com>
2024-12-02 17:00:55 -0500

    change var names

--- PROJ-2/frontend/src/pages/HomePage.tsx
+++ PROJ-2/frontend/src/pages/HomePage.tsx
@@ -38,7 +38,7 @@ const HomePage: React.FC = () => {
 
     const handleSaveSet = async () => {
         if (newSetName.trim()) {
-            const newSet = { userId: Id, title: newSetName, isEditing: false};
+            const newSet = { id: Id, name: newSetName, isEditing: false};
             try {
             await doAddSet(newSet);
             setStudySets([...studySets, newSet]);
