af16184ab73cb71fef889d88397fa1d63a77c37d
oleedee <oneilleedee@gmail.com>
2024-12-02 17:06:37 -0500

    Update HomePage.tsx

--- PROJ-2/frontend/src/pages/HomePage.tsx
+++ PROJ-2/frontend/src/pages/HomePage.tsx
@@ -38,7 +38,7 @@ const HomePage: React.FC = () => {
 
     const handleSaveSet = async () => {
         if (newSetName.trim()) {
-            const newSet = { id: Id, name: newSetName, isEditing: false};
+            const newSet = { userId: Id, title: newSetName};
             try {
             await doAddSet(newSet);
             setStudySets([...studySets, newSet]);
