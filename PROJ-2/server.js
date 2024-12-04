87e936590c820ea0bcec4f43482ad911b392de59
oleedee <oneilleedee@gmail.com>
2024-12-04 16:51:06 -0500

    Update server.js

--- PROJ-2/server.js
+++ PROJ-2/server.js
@@ -118,15 +118,13 @@ app.post('/api/setName', async (req, res) => {
     try {
         const db = client.db('project');
         
-        // Correctly instantiate ObjectId
-        /*
+
         const { ObjectId } = require('mongodb');
         const objectId = new ObjectId(setId); // Use new keyword to create ObjectId instance
         console.log(objectId);
-        */
 
         const updateResult = await db.collection('StudySets').updateOne(
-            { UserId: userId, setName: setId },
+            { UserId: userId, _id: objectId },
             { $set: { SetName: newName } }
         );
 
