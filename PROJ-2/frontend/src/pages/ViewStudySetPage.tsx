fc8aeeec19dabed460fb660415ed0e1fc29d43ce
oleedee <oneilleedee@gmail.com>
2024-12-04 15:23:29 -0500

    Update ViewStudySetPage.tsx

--- PROJ-2/frontend/src/pages/ViewStudySetPage.tsx
+++ PROJ-2/frontend/src/pages/ViewStudySetPage.tsx
@@ -37,10 +37,12 @@ const ViewStudySetPage = () => {
         console.log(fetchedSet.results.Flashcards);
         if (fetchedSet.results) {
             const len = fetchedSet.results.Flashcards.length;
+            /*
               for(int i = 0; i < len; i++) {
                 setTerm(fetchedSet.results.Flashcards[i].term);
                 setDefinition(fetchedSet.results.Flashcards[i].definition);
               }
+            */
         }
     } catch (error) {
         console.error("Failed to load sets", error);
