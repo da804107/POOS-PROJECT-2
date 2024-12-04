6182a779b2cb8f0e1110916efcc8a67de94a8f93
oleedee <oneilleedee@gmail.com>
2024-12-04 16:37:14 -0500

    Update server.js

--- PROJ-2/server.js
+++ PROJ-2/server.js
@@ -122,7 +122,7 @@ app.post('/api/setName', async (req, res) => {
         const objectId = new ObjectId(setId); // Use new keyword to create ObjectId instance
 
         const updateResult = await db.collection('StudySets').updateOne(
-            { UserId: userId, SetName: setId },
+            { UserId: userId, _id: objectId },
             { $set: { SetName: newName } }
         );
 
